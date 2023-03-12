523f149f162b5465fd174bbb3c56418017e031b5
aseppp <saepudinasep86@gmail.com>
2023-03-12 19:56:21 +0700

    remove layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,27 @@ import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import theme from '../styles/theme';
-import Layout from './layout';
+import Navbar from '../components/Navbar';
+import { useRouter } from 'next/router';
+
+import { Rubik } from '@next/font/google';
+
+const rubik = Rubik({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
 
 export default function App({ Component, pageProps, session }) {
+  const router = useRouter();
+
   return (
     <ChakraProvider theme={theme}>
       <SessionProvider session={session}>
-        <Layout>
+        {router.pathname === '/auth' ? null : <Navbar />}
+
+        <main className={rubik.className}>
           <Component {...pageProps} />
-        </Layout>
+        </main>
       </SessionProvider>
     </ChakraProvider>
   );
